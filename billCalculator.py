f59c779452ae5e50c490b515f6e8f6923763a993
Misba Fathima <134159136+MisbaFathima06@users.noreply.github.com>
2025-08-25 06:13:19 +0000

    Initial commit

---
+++ billCalculator.py
@@ -0,0 +1,18 @@
+print("Welcome to Python Pizza Deliveries!")
+size= input("What size do you want?S,M orL?")
+pepperino= input("Do u want pepperino on your pizza? Y or N:")
+extra_cheese= input("Do u want extra cheese on your pizza? Y or N:")
+
+bill=0
+if size=="s|S":
+    bill+=15
+elif size=="m/M"
+    bill=20
+else:
+    bill+= 25
+
+     bill+=2 
+   
+
+
+print("Your final bill is :")
