0aed13c3a5bdcfd81fe600aa622be71e6c343839
Misba Fathima <134159136+MisbaFathima06@users.noreply.github.com>
2025-08-28 07:46:59 +0000

    2nd commit

--- Caesar Cipher.py
+++ Caesar Cipher.py
@@ -10,11 +10,6 @@ print("""
 import string
 alphabets= list(string.ascii_lowercase)
 
-user_choice=input("Type encode/e for Encryption ORRR Type decode/d for Decryption:\n").lower()      
-text =input(f"Enter the text you want to {user_choice}:\n").lower()
-shift=int(input("Type the shift number: "))
-
-
 def caesar(original_text,shift_amount,encode_or_decode):
     output_text="" 
 
@@ -22,7 +17,7 @@ def caesar(original_text,shift_amount,encode_or_decode):
     if encode_or_decode=="decode" or encode_or_decode=="d":
         shift_amount*=-1
         # the shift gets multiplied by - sign:hence goes in backward direction
-
+        #and should be outside for loop bcz inside its for whole encryption and here the shift value gets changed
     for letter in original_text:
         
         if letter in alphabets:             #if letters in original text
@@ -36,9 +31,26 @@ def caesar(original_text,shift_amount,encode_or_decode):
 
     return output_text
 
+should_continue=True
 
-result=caesar(original_text=text, shift_amount=shift, encode_or_decode=user_choice)
-print(f"Here is the {user_choice}d result= {result}")
+while should_continue:
+    user_choice=input("Type encode/e for Encryption ORRR Type decode/d for Decryption:\n").lower()      
+    text =input(f"Enter the text you want to {user_choice}:\n").lower()
+    shift=int(input("Type the shift number: "))
+    
+    result=caesar(original_text=text, shift_amount=shift, encode_or_decode=user_choice)
+    print(f"Here is the {user_choice}d result= {result}")
+#calls the function and prints result everytime we do. So it should be in the loop itself
+    
+    restart=input('Type "yes" if you want to continue else type "no": ')
+    if restart=="no":
+        should_continue=False
+        print("Thanyou!!")
+
+
+
+
+
 
 
         
